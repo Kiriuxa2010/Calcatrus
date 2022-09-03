d37f84fcc0be5c0001da7dcd2241f90da22f3777
Ice2010 <kirillcurachin@gmail.com>
2022-09-03 18:55:49 +0200

    Calcatrus
    Ezy Calculator

---
+++ calcatrus/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+
+
+int main() {
+    long num1; // variables for the numbers and symbol
+    long num2;
+    std::string plus;
+
+
+    std::cout << "Enter your First Number: "; 
+    std::cin >> num1; // get input from user, use the input on the variable num1
+
+    std::cout << "Enter your Second Number: ";
+    std::cin >> num2; // same thing but with var. num2
+
+    std::cout << "Enter Symbol(+, -, * )";
+    std::cin >> plus; // same thing but with var plus
+
+    if (plus == "+") { // check if user uses + - or *
+        std::cout << num1 + num2 << "\n";
+    }
+
+    if (plus == "-"){
+        std::cout << num1 - num2 << "\n";
+    }
+
+    if (plus == "*"){
+        std::cout << num1 * num2 << "\n";
+    }
+
+    return 0; // if the return is 0, the app has no errors, if the return is 1 the app has errors
+}
